7996ed7691dea9ba45fa198608c0e8ae9b2fd9a1
kavindutharaka <ktliyanapathirana2000@gmail.com>
2025-10-09 15:06:50 +0530

    chore: update dependencies and add database connection configuration

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
+const connectDB = require('./config/db');
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3001;
 app.use(express.json());
 
-const payload = { uId : '12', role: 'admin'}
-const secretKey = "lololo";
-const options = { expiresIn: '24h' };
-const token = jwt.sign(payload, secretKey, options);
-console.log(token);
+// const payload = { uId : '12', role: 'admin'}
+// const secretKey = "lololo";
+// const options = { expiresIn: '24h' };
+// const token = jwt.sign(payload, secretKey, options);
+// console.log(token);
+connectDB();
+app.get('/',(req,res) =>{
+    res.send('Hello World!');
+});
 
 
 app.listen(port, ()=>{
     console.log(`Server running on ${port} port!`);
-})
+});
